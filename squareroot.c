37e8b8a2b6282c7d62f1d1faa353a4be18d5d44c
sagarsherchan <sagarsherchan1@gmail.com>
2015-06-18 13:05:56 +0545

    Square Root

---
+++ squareroot.c
@@ -0,0 +1,21 @@
+/* #include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void squareroot(float x);
+
+int main()
+{
+    float x;
+    printf("Enter a number to find its square root \t");
+    scanf("%f",&x);
+    squareroot(x);
+    getch();
+    
+}
+*/
+
+void squareroot(float x)
+{
+                 printf("The square root of given number %f is %f",x,sqrt(x));
+                 
+}
